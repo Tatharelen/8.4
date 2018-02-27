0013375c9ae609761687eb9d9b40e20dedec9985
Michał Sikorski <sikorhurricane@gmail.com>
2018-02-27 12:23:03 +0100

    Add some comments to js script

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+var name = prompt('Enter your name'); // Variables are containers for storing data values. 
+alert('Hello, ' + name); // The alert() method displays an alert box. 
+console.log('Hello, ' + name); 
+
+/* For debugging purposes, 
+you can use the console.log() 
+method to display data. */
+
